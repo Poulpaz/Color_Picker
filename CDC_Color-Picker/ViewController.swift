ba9bebb4cf39998534d3884d43c716890ddd8f06
Tristan <letristandu39@hotmail.fr>
2019-01-24 08:42:27 +0100

    Initial Commit

---
+++ CDC_Color-Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CDC_Color-Picker
+//
+//  Created by lpiem on 24/01/2019.
+//  Copyright © 2019 sge-groupama-fdj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
